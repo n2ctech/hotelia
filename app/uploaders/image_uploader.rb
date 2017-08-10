66ce3a41f2c3aa2af5f7ea6f65e10e4fbc37d0dc
Duc Giang <giangnguyen1089@gmail.com>
2017-08-10 17:10:50 +0700

    config s3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,17 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process resize_to_fill: [200, 200]
   end
 end
