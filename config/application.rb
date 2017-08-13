1265084ac24276716c4ced13d59d3e22e3ff4ba9
Duc Giang <giangnguyen1089@gmail.com>
2017-08-13 21:40:24 +0700

    custom 404 page

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module Hotelia
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    require Rails.root.join("lib/custom_public_exceptions")
+    config.exceptions_app = CustomPublicExceptions.new(Rails.public_path)
   end
 end
