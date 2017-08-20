5b0088d1541c4ab9d8cb2d4ea09bd077ab039152
Ngoc Thoai Nguyen <ngocthoaia1@gmail.com>
2017-08-20 16:45:53 +0700

    fix  position

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,18 +5,18 @@ class PagesController < BaseController
     @home_config = HomeConfig.first
     @slider_warehouse_products =
       scope.where(product_id: @home_config.slider_product_ids)
-      .to_a.sort_by{|e| @home_config.slider_product_ids[e.product_id]}
+      .to_a.sort_by{|e| @home_config.slider_product_ids.index(e.product_id)}
 
     @warehouse_product_list_1 =
       scope.where(product_id: @home_config.product_list_1_ids)
-      .to_a.sort_by{|e| @home_config.product_list_1_ids[e.product_id]}
+      .to_a.sort_by{|e| @home_config.product_list_1_ids.index(e.product_id)}
     @warehouse_product_list_2 =
       scope.where(product_id: @home_config.product_list_2_ids)
-      .to_a.sort_by{|e| @home_config.product_list_2_ids[e.product_id]}
+      .to_a.sort_by{|e| @home_config.product_list_2_ids.index(e.product_id)}
     @categories = @home_config.categories
     @popular_warehouse_products =
       scope.where(product_id: @home_config.popular_product_ids)
-      .to_a.sort_by{|e| @home_config.popular_product_ids[e.product_id]}
+      .to_a.sort_by{|e| @home_config.popular_product_ids.index(e.product_id)}
   end
 
   private
